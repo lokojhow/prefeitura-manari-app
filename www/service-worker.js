5bd0122f990858fd0baef6306f5d67db55a9d5e5
lokojhow <discipulojonathas@gmail.com>
2026-08-17 16:28:31 -0300

    Corrige navegacao e sessao admin V1.2.1

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='manari-v1-2-instagram';
+const CACHE='manari-v1-2-1-scroll-auth';
 const CORE=['./','index.html','styles.css','app.js','config.js','manifest.webmanifest','app-icon-192.png','app-icon-512.png','manari-educacao-conferencia.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
