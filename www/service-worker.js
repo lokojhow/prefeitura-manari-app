6fb2aa6a4f25dced848f0169782c8abdb7e187a8
lokojhow <discipulojonathas@gmail.com>
2026-08-17 21:59:36 -0300

    Atualiza cache Portal Oficial V1.3

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='manari-v1-2-3-nav-fix';
+const CACHE='manari-v1-3-0-portal-oficial';
 const CORE=['./','index.html','styles.css','app.js','config.js','manifest.webmanifest','app-icon-192.png','app-icon-512.png','manari-educacao-conferencia.png'];
 
 self.addEventListener('install',e=>e.waitUntil(
